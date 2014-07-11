1bdaa31c7ce35f6a89ba30a26775356bd640eaed
samarth <samarth@CSDTP-0064.(none)>
2014-07-11 14:50:26 +0530

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "users/new"
 
@@ -13,6 +14,8 @@ SampleApp::Application.routes.draw do
   #get "static_pages/contact"
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
